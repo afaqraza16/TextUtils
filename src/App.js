f7ab8c4275a5c72e7057630c954fa3a826373377
afaqraza16 <afaqraza0220@gmail.com>
2021-11-02 22:30:19 -0700

    Create React App
    my-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
-import logo from './logo.svg';
+
 import './App.css';
+// import About from './Component/About';
+import Navbar from './Component/Navbar';
+import TextForm from './Component/TextForm';
+import React, { useState } from 'react'
+import Alert from './Component/Alert';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+
 
 function App() {
+  const [mode, setMode] = useState('light')
+  const [alert, setAlert] = useState(null)
+    const showAlert =(message, type)=>{
+       setAlert({
+         msg: message,
+         type: type
+       })
+       setTimeout(() => {
+         setAlert(null);
+       }, 1500);
+
+    }
+  const toggleMode=()=>{
+    if(mode === 'light'){
+      setMode ('dark');
+      document.body.style.backgroundColor='#042743';
+      showAlert("Dark has been Enable","success");
+      document.title="Textutils = Dark Mode";
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+      showAlert("Light has been Enable","success");
+      
+      document.title="Textutils = Light Mode";
+    }
+  }
+  const toggleMode1=()=>{
+    if(mode === 'light'){
+      setMode ('dark');
+      document.body.style.backgroundColor='#00584e';
+      showAlert("Green Color has been Enable","success");
+      
+      document.title="Textutils = Green Mode";
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+      showAlert("Light has been Enable","success");
+      
+      document.title="Textutils = Light Mode";
+    }
+  }
+  const toggleMode2=()=>{
+    if(mode === 'light'){
+      setMode ('dark');
+      document.body.style.backgroundColor='#6d564e';
+      showAlert("Gray Color has been Enable","success");
+      
+      document.title="Textutils = Gray Mode";
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+      showAlert("Light has been Enable","success");
+      
+      document.title="Textutils = Light Mode";
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+{/* <Router> */}
+ <Navbar title="Textutils" abouttext= "About" mode={mode}  toggleMode={toggleMode} toggleMode1={toggleMode1} toggleMode2={toggleMode2}/> 
+<Alert alert={alert}/>
+
+<div className="container my-3">
+{/* <Switch> */}
+          {/* <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/"> */}
+ <TextForm showAlert={showAlert} heading="Enter text to Analyze Below" mode={mode}/>
+ {/* </Route> */}
+          
+        {/* </Switch> */}
+
+ </div>
+ {/* </Router> */}
+
+</>
+
+   
   );
 }
 
